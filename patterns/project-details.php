d57a159f4952309debedcd373f43949a1ce7b61a
Gordon Potter <gordon.l.potter@gmail.com>
2023-08-24 15:39:35 -0400

    Add pattern 'project-details'

---
+++ patterns/project-details.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Title: Project Details Section
+ * Slug: twentytwentyfour/project-details
+ * Categories: featured
+ */
+
+?>
+<!-- wp:group {"tagName":"section","align":"wide","layout":{"type":"constrained","wideSize":"1460px"}} -->
+<section class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":"top","style":{"spacing":{"margin":{"top":"5em","bottom":"5em"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-top" style="margin-top:5em;margin-bottom:5em"><!-- wp:column {"verticalAlignment":"top","width":"25%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:25%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.75rem"},"spacing":{"padding":{"top":"2em","right":"4em","bottom":"1em","left":"1em"}}}} -->
+<p style="padding-top:2em;padding-right:4em;padding-bottom:1em;padding-left:1em;font-size:0.75rem">Il mio obiettivo ha svelato l'anima dell'hotel, catturando gli intricati dettagli artigianali dei suoi arredi.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"66.67%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:66.67%"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.3","fontSize":"2.4rem"},"spacing":{"padding":{"top":"0.5em","right":"0.25em","bottom":"1.25em","left":"0em"}}}} -->
+<h3 class="wp-block-heading" style="padding-top:0.5em;padding-right:0.25em;padding-bottom:1.25em;padding-left:0em;font-size:2.4rem;font-style:normal;font-weight:400;line-height:1.3">Una struttura boutique dall'atmosfera artistica, un rifugio che invita i viagjgiatori alla ricerca di un'esperienza autentica e coinvolgente.</h3>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"padding":{"right":"1rem"}}}} -->
+<div class="wp-block-column" style="padding-right:1rem"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">La mia missione era tradurre la miscela unica di arte, artigianato e bellezza naturale dell'hotel in una raccolta di fotografie evocative che risuonassero con gli spettatori e trasmettessero lo spirito di questa incantevole destinazione. Mi sono avvicinato all'incarico con l'obiettivo di catturare il carattere dell'hotel nella sua interezza.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Ogni fotografia è stata meticolosamente composta per armonizzarsi con l'estetica unica dell'hotel, intrecciando senza soluzione di continuità elementi di arte e splendore naturale. Prima di mettere piede sul posto, mi sono impegnato in una ricerca completa per comprendere le sfumature architettoniche, i motivi artistici e il contesto culturale dell'hotel.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></section>
+<!-- /wp:group -->
